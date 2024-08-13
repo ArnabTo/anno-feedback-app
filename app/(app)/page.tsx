becc4dad50113166e2e9af143e39c68c165be9e9
ArnabTo <arnabs929@gmail.com>
2024-08-13 19:50:16 +0600

    added navbar

---
+++ app/(app)/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar/Navbar";
+import Image from "next/image";
+export default function Home() {
+  return (
+   <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+   </div>
+  );
+}
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { useSession, signOut, signIn } from "next-auth/react";
+import { User } from "next-auth";
+import { Button } from "../ui/button";
+
+
+const Navbar = () => {
+
+    const { data: session } = useSession();
+
+    const user: User = session?.user;
+    return (
+        <nav>
+            <div className="flex items-center justify-between mx-10 my-5">
+                <a className="text-zinc-900 text-3xl font-extrabold" href="#">AnnoChat</a>
+                {
+                    session ? (
+                        <div>
+                            <p className="text-black text-xl">Wellcome, <span className="text-bold">{user?.username || user?.email}</span></p>
+                            <Button onClick={() => signOut()}>
+                                Logout
+                            </Button>
+                        </div>
+                    ) : (<div>
+                        <Button onClick={() => signIn()}>Sign In</Button>
+                    </div>)
+                }
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
