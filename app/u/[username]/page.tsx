84e053a9e95514aa66cbab22d01e6f201f9ad6fa
ArnabTo <arnabs929@gmail.com>
2024-08-16 16:29:17 +0600

    created public page

---
+++ app/u/[username]/page.tsx
@@ -0,0 +1,11 @@
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-zinc-950 font-extrabold text-6xl text-center">Wellcome to Annochat</h1>
+            <p className="text-center">Provide your true feedback here. You will be kept anonymous.</p>
+        </div>
+    );
+};
+
+export default page;
